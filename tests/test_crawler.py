073de87706a8cc852bfe402f2b7c4ed661bd29d8
Marcus Pereira <mpereirassa@gmail.com>
2020-03-21 23:36:12 -0300

    make async tests

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,21 +1,25 @@
-from unittest.mock import patch, Mock
-from overwatch.crawler import Crawler
+from unittest.mock import AsyncMock, patch
+
+import pytest
 from bs4 import BeautifulSoup
+
+from overwatch.crawler import Crawler
 from overwatch.parser import Parser
 
 
-@patch("downloader.Downloader")
-def test_crawler(downloader_mock, heroes_html, hero_html, hero2_html, snapshot):
+@pytest.mark.asyncio
+@patch("downloader.Downloader", new_callable=AsyncMock)
+async def test_crawler(downloader_mock, heroes_html, hero_html, hero2_html, snapshot):
     default_url = "https://playoverwatch.com"
-    html1 = Mock(text=heroes_html)
-    html2 = Mock(text=hero_html)
-    html3 = Mock(text=hero2_html)
+    html1 = AsyncMock(text=heroes_html)
+    html2 = AsyncMock(text=hero_html)
+    html3 = AsyncMock(text=hero2_html)
 
     downloader = downloader_mock.return_value
     downloader.get.side_effect = [html1, html2, html3]
 
     crawler = Crawler(default_url, downloader, "pt-br/heroes")
-    content = crawler.download()
+    content = await crawler.download()
     snapshot.assert_match(content._heroes)
 
 
