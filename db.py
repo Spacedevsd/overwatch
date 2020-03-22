80dfd9c0e81f643d3e10daa4d084d91b53da00dc
Marcus Pereira <mpereirassa@gmail.com>
2020-03-21 23:58:11 -0300

    refactory

--- db.py
+++ db.py
@@ -1,5 +1,15 @@
 from pymongo import MongoClient
 
 
-client = MongoClient("mongodb://localhost:27017")
-db = client.overwatch
+class Db:
+    def __init__(self, host="localhost", port="27017"):
+        self._client = MongoClient(f"mongodb://{host}:{port}")
+        self._db = self._client.overwatch
+
+    def insert(self, instance):
+        if not isinstance(instance, dict):
+            raise TypeError("The instance should be a dict type")
+        self._db["heroes"].insert(instance)
+
+    def close(self):
+        self._client.close()
