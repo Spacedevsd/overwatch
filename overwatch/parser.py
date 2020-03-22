99f43f8318ab0172938b139426df1fd56f2a2667
Marcus Pereira <mpereirassa@gmail.com>
2020-03-22 00:51:33 -0300

    create extract_bio method for parser

--- overwatch/parser.py
+++ overwatch/parser.py
@@ -1,4 +1,7 @@
+import re
 from bs4 import BeautifulSoup
+
+GET_REAL_NAME_AND_AGE = r":\s(.+)\,.+:\s?(\d{1,3}|\w+)"
 
 
 class Parser:
@@ -10,6 +13,7 @@ class Parser:
             "description": self.extract_description(html),
             "abilities": self.extract_abilities(html),
             "difficulty": self.extract_difficulty(html),
+            "bio": self.extract_bio(html),
         }
 
     def extract_name(self, response):
@@ -36,3 +40,28 @@ class Parser:
     def extract_url_params(self, response):
         parsed = BeautifulSoup(response.text, "html.parser")
         return parsed.select("a.hero-portrait-detailed")
+
+    def extract_bio(self, response):
+        content = response.find("li", class_="name").span
+        realname, age = re.search(GET_REAL_NAME_AND_AGE, content.text).groups()
+        occupation = self._normalize_extract_content(
+            response.find("li", class_="occupation")
+        )
+        base = self._normalize_extract_content(response.find("li", class_="base"))
+        affiliation = self._normalize_extract_content(
+            response.find("li", class_="affiliation")
+        )
+        stories = response.select("div.hero-bio-backstory p")
+        story = "".join([story.text for story in stories if story.text])
+
+        return {
+            "realname": realname,
+            "age": age,
+            "occupation": occupation,
+            "base": base,
+            "affiliation": affiliation,
+            "story": story,
+        }
+
+    def _normalize_extract_content(self, t):
+        return t.span.text.split(": ")[1]
